7f094db1d35aab35e22c66ea499236f131dac1c0
Hoang Do <hoangdo.dh94@gmail.com>
2016-02-20 11:04:24 +0700

    test

--- restful-java/src/main/java/vn/khmt/restful/UserService.java
+++ restful-java/src/main/java/vn/khmt/restful/UserService.java
@@ -8,7 +8,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import vn.khmt.db.ConnectToSQL;
@@ -23,7 +25,10 @@ public class UserService {
     @GET
     @Path("/{param}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getUser(@PathParam("param") String id){
+    public Response getUser(@PathParam("param") String id, @Context HttpHeaders headers){
+        System.out.println("Get User");
+        String authenticationHeader = headers.getRequestHeader("authentication").get(0);
+        System.out.println("Authentication header = " + authenticationHeader);
         ConnectToSQL conn = new ConnectToSQL("POSTGRESQL", "ec2-54-227-253-228.compute-1.amazonaws.com:5432", "d8viikojj42e3b", "uzufecmqojhnyx", "WPJGueUbd3npLKslU2BEUOmMHx");
         User output = conn.getUser(Integer.parseInt(id));
         return Response.status(200).entity(output).build();
